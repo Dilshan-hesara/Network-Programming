e7bc6456e3fea93c5e4b6ff09143a1ae1cc4b6b2
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-04-01 05:11:24 +0530

    two way communication

--- src/main/java/lk/cw/Sever.java
+++ src/main/java/lk/cw/Sever.java
@@ -2,30 +2,39 @@ package lk.cw;
 
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Sever {
     public static void main(String[] args) {
         System.out.println("sever connected");
 
+        System.out.println("Server started...");
+        Scanner sc = new Scanner(System.in);
+
         try {
-
-            //sever socket
-
             ServerSocket serverSocket = new ServerSocket(4000);
-
-            //local socket
             Socket socket = serverSocket.accept();
-            System.out.println("client connected");
+            System.out.println("Client connected...");
 
+
+            //get client msg
             DataInputStream in = new DataInputStream(socket.getInputStream());
-            String msg = in.readUTF();
+            String clMsg = in.readUTF();
+            System.out.println("Client: " + clMsg);
 
-            System.out.println(msg);
+            // Send client msg
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            System.out.print("Enter Server Response: ");
+            String response = sc.nextLine();
+            out.writeUTF(response);
+            out.flush();
 
             socket.close();
+            serverSocket.close();
 
         } catch (IOException e) {
             throw new RuntimeException();
--- src/main/java/lk/cw/client.java
+++ src/main/java/lk/cw/client.java
@@ -1,23 +1,40 @@
 package lk.cw;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class client {
     public static void main(String[] args) {
 
         //remote socket
 
+        Scanner sc = new Scanner(System.in);
+
         try {
+
+            //send msg sever
             Socket socket = new Socket("localhost", 4000 );
-            String msg ="whtyggyy";
+            String msg;
+
+            System.out.print("Enter Msg c  :");
+            msg = sc.nextLine();
+
             DataOutputStream out =new DataOutputStream(socket.getOutputStream());
             out.writeUTF(msg);
             out.flush();//msg send
-            socket.close();
 
+
+            //get sever msg
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            String serverResponse = in.readUTF();
+            System.out.println("Server: " + serverResponse);
+
+            socket.close();
         }
         catch (IOException e) {
             throw new RuntimeException();
